26dd6280a9f82abb2ebe5441ea0888041a44863c
Shubham Shukla <shubham.shukla@cocodelivery.com>
2022-03-23 14:30:49 -0400

    working with complete lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,9 +1,69 @@
-exports.handler = async function(event) {
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: `Hello, CDK! You've hit ${event.path}\n`
+const https = require('https');
+
+function postRequest(body) {
+    const options = {
+        hostname: 'order.delivery-dev.cocodelivery.com',
+        path: '/api/v2/orders/three-pl',
+        method: 'POST',
+        port: 443,
+        headers: {
+            'Content-Type': 'application/json',
+        },
     };
-  };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                console.log('statusCode:', res.statusCode);
+                res.on('data', chunk => {
+                    console.log("chunk:", JSON.parse(chunk));
+                });
+                return reject(new Error(res.body, 'statusCode=' + res.statusCode));
+            } else {
+
+                const response = {
+                    statusCode: res.statusCode
+                }
+                return resolve(response);
+            }
+
+        });
+
+        req.on('error', err => {
+            reject(new Error(err));
+        });
+
+        req.write(body);
+        req.end();
+    });
+}
+
+exports.handler = async event => {
+    try {
+        let body;
+        event.Records.forEach(record => {
+            body = record.body;
+            console.log("body:", body);
+        });
+
+        const result = await postRequest(body);
+        
+        console.log('result is: 👉️', result);
+
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(result),
+        };
+    } catch (error) {
+        console.log('Error is: 👉️', error);
+        return {
+            statusCode: 400,
+            body: error.message,
+        };
+    }
+};
   
